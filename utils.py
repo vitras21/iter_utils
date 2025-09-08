bf90209a8979765ec0097516d637c7da2ad5ab48
ion727 <151961936+ion727@users.noreply.github.com>
2025-09-08 22:41:33 +0200

    Fixed min/max + added map dict searching

--- utils.py
+++ utils.py
@@ -7,37 +7,68 @@ def clamp(iterable, index):
         raise ValueError(f"Invalid type for index: expected <int>, got {type(index)}.")
     return min(max(0,index), len(iterable)-1)
 
-
 def is_iterable(obj, exclude_string=False):
+    "Returns True if obj is an iterable, optionally rejecting strings if specified"
     reject_is_string = isinstance(obj, (str, bytes, bytearray)) and exclude_string
     return (isinstance(obj, Iterable) and not reject_is_string)
 
-def deep_max(iterable, index=0):
+def deep_max(iterable, index=0, dtype=int):
+    "Walks through iterable tree to find the highest value of type `dtype`, defaulting to <int>."
+    
     left = iterable[index]
     if is_iterable(left, exclude_string=True):
         left = deep_max(left)
     if index == len(iterable) - 1:
         return left
-    right = deep_max(iterable, index+1)
+    
+    right = iterable[index:]
+    if is_iterable(right, exclude_string=True):
+        right = deep_max(iterable, index+1)
+    
+    if not isinstance(left, dtype):
+        return right if isinstance(right, dtype) else None
+    if not isinstance(right, dtype):
+        return left
+
     print(f"Comparing {left} and {right}")
     return max(left, right)
 
-def deep_max(iterable, index=0):
+def deep_min(iterable, index=0, dtype=int):
+    "Walks through iterable tree to find the lowest value of type `dtype`, defaulting to <int>."
+    
     left = iterable[index]
     if is_iterable(left, exclude_string=True):
-        left = deep_max(left)
+        left = deep_min(left)
+    # ensure list length not exceeded
     if index == len(iterable) - 1:
         return left
-    right = deep_max(iterable, index+1)
-    print(f"Comparing {left} and {right}")
-    return max(left, right)
 
-def recursive_replace(iterable, target_type):
+    right = iterable[index:]
+    if is_iterable(right, exclude_string=True):
+        right = deep_min(iterable, index+1)
+
+    if not isinstance(left, dtype):
+        return right if isinstance(right, dtype) else None
+    if not isinstance(right, dtype):
+        return left
+
+    print(f"Comparing {left} and {right}")
+    return min(left, right)
+
+def deep_replace(iterable, target_type):
     if isinstance(iterable, dict):
         iterable = iterable.items()
     iterable = list(iterable)
     for index, item in enumerate(iterable):
         if is_iterable(item, exclude_string=True):
-            iterable[index] = recursive_replace(item, target_type)
+            iterable[index] = deep_replace(item, target_type)
     return target_type(iterable)
 
+def find_match(key, dict_map=None):
+    """Searches for an item using a key and map, raising an error if not found."""
+    if not isinstance(dict_map, dict):
+        raise ValueError(f"Invalid type for dict_map: expected <dict>, got {type(dict_map)}.")
+    if key not in dict_map.keys():
+        return None
+    return dict_map[key]
+    
